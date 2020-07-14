e6612ca4e137d04b189db27e7e81f20f0b721c69
Joao Victor Ramires Guimaraes Brito <joaoramires.engsoft@gmail.com>
2020-07-14 17:05:58 -0300

    ADD User controller and Tests

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -5,7 +5,5 @@ class ApplicationHelperTest < ActionView::TestCase
   test "full title helper" do
     assert_equal full_title,            'Ruby on Rails Tutorial'
     assert_equal full_title("Help"),    'Help | Ruby on Rails Tutorial'
-    assert_equal full_title("Contact"), 'Contact | Ruby on Rails Tutorial'
-    assert_equal full_title("About"),   'About | Ruby on Rails Tutorial'
   end
 end
