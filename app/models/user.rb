dad89d9000254bc54a2efaf237e08a82571b5a09
Joao Victor Ramires Guimaraes Brito <joaoramires.engsoft@gmail.com>
2020-07-15 14:18:11 -0300

    ADD before save callback to handle email casing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
+  before_save { email.downcase! }
+
   validates :name, presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  # URI::MailTo::EMAIL_REGEXP
-  validates :email, presence: true, uniqueness: { case_sensitive: false },
-                    length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
+  # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true,
+                    length: { maximum: 255 }, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
