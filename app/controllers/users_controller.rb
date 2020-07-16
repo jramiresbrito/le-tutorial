f957497a02dff6db079e982eb99f335479a2c767
Joao Victor Ramires Guimaraes Brito <joaoramires.engsoft@gmail.com>
2020-07-16 20:40:54 -0300

    Add valid signup integration test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      # handle success
+      flash[:success] = 'Welcome to the Jungle!'
+      redirect_to @user
     else
       render :new
     end
