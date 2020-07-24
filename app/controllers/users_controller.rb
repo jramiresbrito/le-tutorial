6efda62eff83b30db6c61f8efb332a51d5041769
Joao Victor Ramires Guimaraes Brito <joaoramires.engsoft@gmail.com>
2020-07-24 06:13:29 -0300

    Feature: ADD Friendly forwarding

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,12 +40,14 @@ class UsersController < ApplicationController
 
   def logged_in_user
     unless logged_in?
+      store_location
       flash[:danger] = 'Please log in.'
       redirect_to login_url
     end
   end
 
   def correct_user
+    set_user
     redirect_to(root_url) unless current_user?(@user)
   end
 
